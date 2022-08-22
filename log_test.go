017ebe8606b05ef8605a7b283314b707464a7791
Dils Matchanov <dils.matchanov@gmail.com>
2022-08-22 14:11:41 +0500

    initialized go module and wrote a test

--- log_test.go
+++ log_test.go
@@ -1,1 +1,17 @@
 package log
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestCycle(t *testing.T) {
+	l := New(os.Stdout, false)
+	ctx := context.Background()
+	l.Info(ctx, "test", P("param", "value"))
+	l.Debug(ctx, "test", P("param", "value"))
+	l.Err(ctx, "test", errors.New("some err"), P("param", "value"))
+	l.Warn(ctx, "test", P("param", "value"))
+}
